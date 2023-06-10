0ea57ad6e9951e5ee627f9aeda2f0c80e9351d80
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-10 01:28:52 +0100

    add bubblesort

--- src/Algorithms/bubbleSort.js
+++ src/Algorithms/bubbleSort.js
@@ -1,21 +1,27 @@
 const delay = (time) => new Promise(resolve => setTimeout(resolve, time))
 
-const bubbleSort = async (array, setArray) => {
-    const len = array.length
-    if (len <= 1) return array
+const bubbleSort = async (arr, setterFunc, setActiveIndex, delayMilliSeconds) => {
+    const len = arr.length
+    if (len <= 1) return arr
 
         for (let i=0; i<len; i++) {
             for (let j=0; j<len-i-1; j++) {
-                await delay(500)
-                if (array[j] > array[j+1]) {
-                    [ array[j], array[j+1] ] = [ array[j+1], array[j] ]
+                await delay(delayMilliSeconds)
+                if (arr[j] > arr[j+1]) {
+                    [ arr[j], arr[j+1] ] = [ arr[j+1], arr[j] ]
                 }
-                setArray(array)
-                console.log(array)
+                setActiveIndex([j, j+1])
+                // pass in new array ∵ react compares by reference
+                setterFunc([...arr])
             }
         }
 
-    // return array
+        for (let i=0; i<len; i++) {
+          await delay(delayMilliSeconds)
+          setActiveIndex([i])        
+        }
+        setActiveIndex([]) 
+
 }
 
 export default bubbleSort
