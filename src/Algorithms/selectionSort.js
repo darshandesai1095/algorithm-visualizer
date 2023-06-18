9c6a86d016be5dc53bad622516fcbd5f505e4f65
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-18 15:08:46 +0100

    selection sort

--- src/Algorithms/selectionSort.js
+++ src/Algorithms/selectionSort.js
@@ -4,29 +4,29 @@ const selectionSort = async (arr, updateArray, setActiveIndex, delayMilliSeconds
     const len = arr.length
     if (len <= 1) return arr
 
-        for (let i=0; i<len-1; i++) {
-            let currentIndex = i
-            let minIndex = i+1
-            for (let j=i+1; j<len; j++) {
-                await delay(delayMilliSeconds)
-                // find minimum
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j
-                }
+    for (let i=0; i<len-1; i++) {
+        let currentIndex = i
+        let minIndex = i+1
+        for (let j=i+1; j<len; j++) {
+            // find minimum
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j
             }
-            if (arr[currentIndex] > arr[minIndex]) {
-                [ arr[currentIndex], arr[minIndex] ] = [ arr[minIndex], arr[currentIndex] ]
-            }
-            setActiveIndex([currentIndex, minIndex])
-            // pass in new array ∵ react compares by reference
-            updateArray([...arr])
         }
+        if (arr[currentIndex] > arr[minIndex]) {
+            [ arr[currentIndex], arr[minIndex] ] = [ arr[minIndex], arr[currentIndex] ]
+        }
+        setActiveIndex([currentIndex, minIndex])
+        await delay(delayMilliSeconds)
+        // pass in new array ∵ react compares by reference
+        updateArray([...arr])
+    }
 
-        for (let i=0; i<len; i++) {
-          await delay(delayMilliSeconds)
-          setActiveIndex([i])        
-        }
-        setActiveIndex([]) 
+    for (let i=0; i<len; i++) {
+        setActiveIndex([i])     
+        await delay(delayMilliSeconds)   
+    }
+    setActiveIndex([]) 
 
 }
 
