989e6f9d19f526385f0d80f5406a495339d78520
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-20 17:07:47 +0100

    test quicksort

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Element from './components/Element/Element';
 import bubbleSort from './Algorithms/bubbleSort';
 import selectionSort from './Algorithms/selectionSort';
 import insertionSort from './Algorithms/insertionSort';
-import mergeSort from './Algorithms/mergeSort'; // replaced with iterative version
-import mergeSortIterative from './Algorithms/mergeSortIterative';
+import mergeSort from './Algorithms/mergeSort';
 import heapSort from './Algorithms/heapSort';
+import cocktailSort from './Algorithms/cocktailSort';
+import gravitySortInPlace from './Algorithms/gravitySort';
+import quickSort from './Algorithms/quickSort';
 
 
 const App = () => {
@@ -14,10 +16,10 @@ const App = () => {
   const [delay, setDelay] = useState(50)
   const [activeIndex, setActiveIndex] = useState([])
   const [currentArray, setCurrentArray] = useState(
-    [39, 12, 44, 28, 6, 22, 18, 7, 49, 48, 15, 50, 42, 14, 16, 26, 36, 1, 29, 34, 40, 25, 43, 2, 9, 38, 24, 13, 4, 46, 35, 3, 19, 10, 17, 23, 31, 8, 20, 5, 45, 30, 47, 32, 37, 11, 21, 27, 33, 41]
+    [39, 12, 44, 28, 6, 22, 18, 7, 49, 48, 15, 50, 42, 14, 16, 26, 36, 1, 29, 34, 40, 25, 43, 2, 9, 38, 24, 13, 4, 46, 35, 3, 19, 10, 17, 23, 31, 8, 20, 5, 45, 30, 47, 32, 37, 11, 21, 27, 33, 41,  44, 28, 6, 22, 18, 7, 49, 48, 15, 50, 42, 14, 16, 26, 36, 1, 29, 34, 40, 25, 43, 2, 9, 38, 24, 13, 4, 46, 35]
   )
   useEffect(() => {
-    heapSort(currentArray, setCurrentArray, setActiveIndex, delay)
+    quickSort(currentArray, setCurrentArray, setActiveIndex, delay)
   }, [])
 
 
