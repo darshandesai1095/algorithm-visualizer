89de26da64801e2fc24c19e5735496da2cc54797
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-24 15:46:54 +0100

    add setmenuvisible props

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,14 +1,16 @@
 import MenuItem from '../MenuItem/MenuItem';
 import './Menu.css';
 
-const Menu = ({ menuVisible }) => {
+const Menu = ({ menuVisible, setMenuVisible }) => {
 
     const menuItems = [
         "Bubble Sort", "Cocktail Sort", "Heap Sort", "Insertion Sort",
         "Merge Sort", "Shell Sort", "Selection Sort", "Quick Sort"
         ]
 
-    const renderMenuItems = menuItems.map( item => (<MenuItem key={item} item={item}/>) )
+    const renderMenuItems = menuItems.map(item => (
+        <MenuItem key={item} algorithmName={item} setMenuVisible={setMenuVisible}/>
+    ))
 
     return (
         <div className={`menu ${menuVisible ? "visible" : "hidden"}`}>
