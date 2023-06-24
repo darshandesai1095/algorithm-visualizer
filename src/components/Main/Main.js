4df8bf2fe4d41b6a24177610eedd6862403df5d9
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-24 15:46:21 +0100

    create main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import './Main.css';
+import MetaData from '../MetaData/MetaData';
+import Visualization from '../Visualization/Visualization';
+
+const Main = () => {
+
+  return (
+    <div className="main">
+        <MetaData/>
+        <Visualization/>
+    </div>
+  )
+}
+
+export default Main;
