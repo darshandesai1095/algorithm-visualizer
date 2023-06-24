2a2a0c641f720902acb5716b23d333488255d901
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-24 15:48:05 +0100

    close menu when new function is selected

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,17 +1,27 @@
 import './MenuItem.css';
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { setAlgorithm } from '../../features/algorithm/algorithmSlice';
 
-const MenuItem = ({ item }) => {
+const MenuItem = ({ algorithmName, setMenuVisible }) => {
+
+    const currentAlgorithm = useSelector(state => state.algorithm.value)
+    const dispatch = useDispatch()
+    const handleClick = () => {
+        dispatch(setAlgorithm(algorithmName))
+        setMenuVisible(false)
+    }
 
     const [isHovered, setIsHovered] = useState(false)
 
     return (
         <div 
-            className="menu-item"
+            className={`menu-item ${currentAlgorithm === algorithmName ? "active-algorithm" : null}`}
+            onClick={handleClick}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
-            <p>{item}</p>
+            <p>{algorithmName}</p>
             <div className={`${isHovered ? "highlight" : "unhighlight"}`} >
             </div>
         </div>
