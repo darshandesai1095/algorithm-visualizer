9884b01a97626684eae0c53fda90278ee892e1a9
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-24 15:49:36 +0100

    create delay slice to track delay

---
+++ src/features/delaySlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    value: 5
+}
+
+export const delaySlice = createSlice({
+  name: 'delay',
+  initialState,
+  reducers: {
+    increment: (state) => {
+        if (state.value >= 10) {
+            state.value = state.value + 10
+        }
+        if (state.value === 5) {
+            state.value = state.value + 5
+        } 
+    },
+    decrement: (state) => {
+        if (state.value === 10) {
+            state.value = state.value - 5
+        }
+        if (state.value >= 10) {
+            state.value = state.value - 10
+        }
+        if (state.value === 5) {
+            state.value = 5
+        }
+    },
+  },
+})
+
+export const { increment, decrement } = delaySlice.actions
+
+export default delaySlice.reducer
