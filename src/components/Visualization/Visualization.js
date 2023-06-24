ff160abb8362853584b731f6929805ec9f782332
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-24 15:49:22 +0100

    add visualization component

---
+++ src/components/Visualization/Visualization.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import './Visualization.css';
+import Element from '../Element/Element';
+import { useSelector } from 'react-redux';
+import mapAlgorithm from '../../functions/mapAlgorithm';
+
+
+const Visualization = () => {
+
+    const sortingAlgorithm = useSelector(state => state.algorithm.value)
+    const toggleRerun = useSelector(state => state.toggleRerun.value)
+    const delay = useSelector(state => state.delay.value)
+
+    const [arrayLength, setArrayLength] = useState(100)
+    const [activeIndex, setActiveIndex] = useState([])
+    const [currentArray, setCurrentArray] = useState([])
+
+    useEffect(() => {
+        let sort = mapAlgorithm(sortingAlgorithm)
+        sort(setCurrentArray, setActiveIndex, delay, arrayLength)
+    }, [sortingAlgorithm, toggleRerun])
+
+    
+    const elements = currentArray.map((el, index) => {
+        return (
+            <Element
+                key={index}
+                orderNo={index}
+                width={0.7}
+                height={el*6}
+                active={activeIndex.includes(index) ? true : false}
+                sorted={false}
+            />
+        )
+    })
+
+    return (
+        <div className="visualization">
+            {elements}
+        </div>
+    )
+}
+
+export default Visualization;
