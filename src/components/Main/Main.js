8a88ebd5fd482e216993600fa1f81e1877a7cc7e
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-25 15:14:43 +0100

    add meta data

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,21 @@
 import './Main.css';
+import { useState } from 'react';
 import MetaData from '../MetaData/MetaData';
 import Visualization from '../Visualization/Visualization';
 
 const Main = () => {
 
+  const [metaData, setMetaData] = useState({
+    iterations: 0,
+    comparisons: 0,
+    swaps: 0,
+    shifts: 0
+})
+
   return (
     <div className="main">
-        <MetaData/>
-        <Visualization/>
+        <MetaData metaData={metaData}/>
+        <Visualization setMetaData={setMetaData} metaData={metaData}/>
     </div>
   )
 }
