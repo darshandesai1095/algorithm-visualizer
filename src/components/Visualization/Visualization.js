86b1870faf13ce8ac7f053d0f803a6e586d5dc7f
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-25 23:20:50 +0100

    format code

--- src/components/Visualization/Visualization.js
+++ src/components/Visualization/Visualization.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './Visualization.css';
 import Element from '../Element/Element';
 import { useSelector } from 'react-redux';
@@ -10,22 +10,42 @@ const Visualization = (props) => {
     const sortingAlgorithm = useSelector(state => state.algorithm.value)
     const toggleRerun = useSelector(state => state.toggleRerun.value)
     const delay = useSelector(state => state.delay.value)
-
     const [arrayLength, setArrayLength] = useState(100)
     const [activeIndex, setActiveIndex] = useState([])
     const [currentArray, setCurrentArray] = useState([])
 
-
     useEffect(() => {
-        let sort = mapAlgorithm(sortingAlgorithm)
-        sort(setCurrentArray, setActiveIndex, delay, arrayLength, props.setMetaData, {
-            iterations: 0,
-            comparisons: 0,
-            swaps: 0,
-            shifts: 0
-        })
-
-    }, [sortingAlgorithm, toggleRerun])
+        let isCancelled = false
+      
+        const runSort = async () => {
+          try {
+            let sort = mapAlgorithm(sortingAlgorithm)
+            await sort(
+              setCurrentArray,
+              setActiveIndex,
+              delay,
+              arrayLength,
+              props.setMetaData,
+              {
+                iterations: 0,
+                comparisons: 0,
+                swaps: 0,
+                shifts: 0
+              },
+              () => isCancelled // Pass the cancellation check function to the sort function
+            )
+          } catch (error) {
+            console.log("it's not working")
+          }
+        }
+      
+        runSort()
+      
+        return () => {
+          isCancelled = true
+        }
+      }, [sortingAlgorithm, toggleRerun])
+      
 
     
     const elements = currentArray.map((el, index) => {
